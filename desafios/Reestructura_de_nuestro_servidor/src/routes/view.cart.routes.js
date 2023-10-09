eeedb9f2be16eb5d42fd847013255196e464de74
David <david.acerbo@gmail.com>
2023-10-09 08:34:06 -0300

    se ajusta el router de cart view

--- desafios/Reestructura_de_nuestro_servidor/src/routes/view.cart.routes.js
+++ desafios/Reestructura_de_nuestro_servidor/src/routes/view.cart.routes.js
@@ -1,10 +1,36 @@
 import {Router} from 'express';
-import {getACartByIdWebFront} from "../controllers/view.cart.controller.js";
 import {requiredLoginSession} from "./utils/utils.js"
+import cartManager from "../dao/selectedCartDb.js";
 
 const router = Router();
 
 // GET Render WEB HTML CART
-router.get("/:cid", requiredLoginSession, getACartByIdWebFront);
+router.get("/:cid", requiredLoginSession, async(req, res) => {
+    let {cid} = req.params;
+    const user = req.session.user;
+    try {
+        const response = await cartManager.getCartById(cid);
+        const products = response?.products.map(product => {
+                return {
+                    ...product['productId'].toJSON(),
+                    qty: product['qty']
+                }
+            }
+        );
+
+        res.render('cart_products', {
+            data: products,
+            cart_id: cid,
+            user,
+            sessionActive: req.session.user ? true : false,
+            style: 'home.css'
+        });
+    } catch (error) {
+        res.render('error',
+            {
+                error: JSON.stringify(error)
+            });
+    }
+});
 
 export default router;
