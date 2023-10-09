4b4f8b6b99f2b0868322bae3a7bd6661a685ac6e
David <david.acerbo@gmail.com>
2023-10-09 08:48:45 -0300

    se ajusta router de la view users

--- desafios/Reestructura_de_nuestro_servidor/src/routes/view.users.routers.js
+++ desafios/Reestructura_de_nuestro_servidor/src/routes/view.users.routers.js
@@ -1,13 +1,27 @@
 import { Router } from 'express';
-import * as UsersController from "../controllers/view.user.controller.js";
 import {requiredLoginSession, authToken} from "./utils/utils.js"
 
 const router = Router();
 
-router.get("/register/", UsersController.register);
+router.get("/register/", async (req, res)=> {
+    return res.render('register', {
+        style: 'main.css'
+    });
+});
 
-router.get("/login/",  UsersController.login);
+router.get("/login/",  async (req, res) => {
+    return res.render('login', {
+        style: 'main.css'
+    });
+});
 
-router.get("/", requiredLoginSession, UsersController.profile);
+router.get("/", requiredLoginSession, async (req, res) => {
+    const user =  req.session.user;
+    return res.render('profile', {
+        style: 'main.css',
+        sessionActive: req.session.user ? true : false,
+        user
+    });
+});
 
 export default router;
