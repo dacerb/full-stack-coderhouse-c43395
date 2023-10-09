b6029620cf0a3783ae132cf282c0d772703c687f
David <david.acerbo@gmail.com>
2023-10-09 08:37:32 -0300

    se ajusta router de home

--- desafios/Reestructura_de_nuestro_servidor/src/routes/view.home.routes.js
+++ desafios/Reestructura_de_nuestro_servidor/src/routes/view.home.routes.js
@@ -1,10 +1,20 @@
 import { Router } from 'express';
-import * as HomeController from "../controllers/view.home.controllers.js";
 import {requiredLoginSession} from "./utils/utils.js"
+import productManager from "../dao/selectedProductDb.js";
 
 const router = Router();
 
 // GET HOME
-router.get("/", requiredLoginSession, HomeController.getHome);
+router.get("/", requiredLoginSession, async(req, res) => {
+    const user = req.session.user;
+    const all_products = await productManager.getProducts();
+    res.render('home', {
+        qty: all_products.length,
+        user,
+        sessionActive: req.session.user,
+        data: all_products,
+        style: 'home.css'
+    });
+});
 
 export default router;
