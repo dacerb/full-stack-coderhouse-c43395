42a0d04a6c0d9051391196b8331c6f771095b0e0
David <david.acerbo@gmail.com>
2023-12-03 16:36:01 -0300

    se completa la entrega

--- desafios/testing_avanzado/test/supertest.test.js
+++ desafios/testing_avanzado/test/supertest.test.js
@@ -1,4 +1,5 @@
 import chai from "chai";
+const assert = chai.assert;
 import supertest from "supertest";
 
 
@@ -9,16 +10,16 @@ const expect = chai.expect;
 const requester = supertest('http://localhost:8080');
 
 // Aumentar el timeout global
+before(function() {
+    this.timeout(5000);
+});
 
-
-// Contexto Global
 describe("testing e-commerce app:", () => {
 
     // TEST BLOCK 1
     describe("* products", () => {
 
         before(async function() {
-            this.timeout(5000);
 
             this.cookie;
             this.mockAdminUser = {
@@ -82,12 +83,87 @@ describe("testing e-commerce app:", () => {
     });
 
     // TEST BLOCK 2
-    describe("CARTS", () => {
-        // Puedes agregar pruebas relacionadas con carritos de compras aquí
+    describe("* carts", () => {
+
+        before(async function() {
+
+            this.new_cart_id = 0
+            this.mockAdminUser = {
+                "email": "test_user_mock.test_last_name_mock@mock.com",
+                "password": "mock_password"
+            }
+            this.login_response = await requester.post("/api/sessions/login").send(this.mockAdminUser);
+            this.setCookieHeader = this.login_response.header['set-cookie'][0];
+
+        });
+
+        // 01
+        it("create a new cart", async function() {
+
+            const response = await requester.post("/api/cart/");
+            const {statusCode, _body: body} = {...response};
+
+            expect(statusCode).is.eqls(200);
+            expect(body).to.have.property('message', `success`);
+
+            if (statusCode === 200) this.new_cart_id = body.resutl._id
+        });
+
+        // 02
+        it("get the new cart", async function() {
+
+            const response = await requester.get(`/api/cart/${this.new_cart_id}`);
+            const {statusCode, _body: body} = {...response};
+
+            expect(statusCode).is.eqls(200);
+            expect(body).to.have.property('message', `success`);
+            expect(body.response.products).to.be.an('array');
+
+        });
+
+        // 03
+        it("add product in the new cart", async function() {
+
+            const response = await requester.get("/api/products");
+            const products = response._body.payload;
+
+            assert.isNotEmpty(products)
+            if (products) {
+                const product_to_test = products.pop()
+
+                const response = await requester
+                    .post(`/api/cart/${this.new_cart_id}/product/${product_to_test._id}`)
+                    .set('Cookie', this.setCookieHeader);
+
+                const {statusCode, _body: body} = {...response};
+                const response_message_data = JSON.parse(response.res.text);
+
+                expect(statusCode).is.eqls(200);
+                expect(response_message_data).to.have.property('message', `the product with id ${product_to_test._id} was update in the cart id ${this.new_cart_id}`);
+            }
+
+        });
+
+        // 04
+        it("delete all products in the new cart", async function() {
+            const response = await requester
+                .delete(`/api/cart/${this.new_cart_id}`)
+                .set('Cookie', this.setCookieHeader);
+            const {statusCode, _body: body} = {...response};
+
+            expect(statusCode).is.eqls(200);
+            expect(body).to.have.property('message', `success`);
+        });
+
+        // 05
+        it("delete the new cart", async function() {
+            // PENDIENTE IMPLEMENTAR ALGUN METODO PARA HACERLO
+        });
+
     });
 
     // TEST BLOCK 3
-    describe("* essions", () => {
+    describe("* sessions", () => {
 
         before(async function() {
 
@@ -99,8 +175,6 @@ describe("testing e-commerce app:", () => {
                 "age": 9999,
                 "password": "mock_password"
             }
-
-
         });
 
         // 01
@@ -140,5 +214,11 @@ describe("testing e-commerce app:", () => {
             expect(statusCode).is.eqls(200);
             expect(body).to.have.property('message', `Sesion cerrada correctamente.`);
         });
+
+        //04
+        it("delete the mock user", async function() {
+            // PENDIENTE IMPLEMENTAR ALGUN METODO PARA HACERLO
+        });
     });
+
 });
