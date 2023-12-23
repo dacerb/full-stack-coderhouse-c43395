ba9b0ac3d22251f7789149b884972fef2dc238b1
dacerbo <david.acerbo@mercadolibre.com>
2023-12-23 13:25:35 -0300

    se inicia con el metodo de delete

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -2,11 +2,58 @@ import { userManager  } from "../services/factory.js";
 import UsersDto from '../services/dto/users.dto.js'
 
 export async function deleteUser(req, res, next) {
-    return res.send(
-        {
-            message: "hola deleteUser"
-        }
-    );
+    const { logger } = req;
+    const allUsers = await userManager.getAllUsers();
+    let { limitSeconds } = req.query; // recibo por parametros el limite en segundos
+    if (isNaN(limitSeconds)) return res.status(400).send({
+            status:'error',
+            message: "the limitSeconds must be integer."
+        });
+
+    if (!limitSeconds) {
+        limitSeconds = 60; //de no recibir el parametro el valor por defecto es..
+    }else {
+        limitSeconds = parseInt(limitSeconds);
+    }
+
+    if (!allUsers.length > 0) {
+        return res.status(404).send(
+            JSON.stringify({
+                status: "success",
+                mensaje: "not found any users"
+            }, null, 4));
+    }
+
+    try {
+        const usersToDelet = allUsers.filter(user => {
+            const currentDate = new Date();
+            const userLastLogin = new Date(user.lastLogin);
+            const currentDateTimestamp = currentDate.getTime();
+            const TimestampUserLastLogin = userLastLogin.getTime() + limitSeconds;
+
+            const elapseTime = parseInt((currentDateTimestamp - TimestampUserLastLogin));
+
+            console.log(TimestampUserLastLogin)
+            console.log(currentDateTimestamp)
+            console.log(elapseTime)
+
+            if (elapseTime > limitSeconds) return true;
+
+        })
+
+        return res.status(200).send(
+                {
+                    limitSeconds,
+                    qtyUsersDeleted: usersToDelet.length,
+                    usersDeleted: usersToDelet
+
+                }
+        );
+
+    }catch (error) {
+        logger.error(error)
+        next(error)
+    }
 }
 
 export async function getAllUsers(req, res, next) {
